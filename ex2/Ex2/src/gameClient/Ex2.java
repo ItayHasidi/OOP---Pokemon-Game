be0b99a964e217397fc8b8029e359cd5921e9e03
ItayHasidi <74491361+ItayHasidi@users.noreply.github.com>
2020-12-20 19:25:23 +0200

    Update Ex2.java

--- ex2/Ex2/src/gameClient/Ex2.java
+++ ex2/Ex2/src/gameClient/Ex2.java
@@ -158,6 +158,7 @@ public class Ex2 extends JFrame implements Runnable {
 
         Integer[] arr = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
         lvlSelCombo = new JComboBox(arr);
+        lvlSelCombo.setEditable(true);
         lvlSelCombo.setBounds(110,5,50,25);
         lvlSelCombo.addActionListener(new ActionListener() {
             @Override
